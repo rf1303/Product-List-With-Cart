ab1ab02392da195238697e7d3b023910273f5796
Ramiro Fernandez <pulgaramiro@gmail.com>
2026-01-29 16:31:26 -0300

    modal agegando items

--- src/ConfirmModal.jsx
+++ src/ConfirmModal.jsx
@@ -5,16 +5,11 @@ import { useAddCart } from './data-context/CartOrderData.jsx';
 export const ConfirmOrderModal = ({ open, onClose }) => {
     const confirmModalRef = useRef(null)
     const { state, dispatch } = useAddCart();
-
+    console.log('state:', state)
     useEffect(() => {
         const confirm = confirmModalRef.current;
-        console.log('confirm:', confirm)
-        console.log('confirmModalRef.current:', confirmModalRef.current)
-        console.log('open iniUse:', open)
         if (!confirm) return;
-        console.log('Modal state - open prop:', open, 'dialog.open:', confirm.open);
         if (open && !confirm.open) {
-            console.log('confirm open:', confirm)
             confirm.showModal();
         }
         if (!open && confirm.open) {
@@ -26,13 +21,19 @@ export const ConfirmOrderModal = ({ open, onClose }) => {
     return (
         <dialog ref={confirmModalRef} onClose={onClose}
             onClick={(e) => { if (e.target === confirmModalRef.current) onClose(); }}
-            className='fixed inset-0 w-full bg-white backdrop:bg-balck/20 grid items-end p-6 pt-10'>
-            <header className=''>
-                <span className='w-10.5 h-10.5'><IconConfirmed /></span>
-                <h3 className='text-rose-900 text-preset-1 font-bold capitalize'>order confirmed</h3>
-                <p className='text-rose-500 text-preset-3 font-normal'>We hope you enjoy your food!</p>
-            </header>
-            <button type='button' onClick={onClose} className='w-full py-4 bg-red text-white text-preset-3 font-bold capitalize rounded-full'>start new order</button>
+            className='fixed inset-0 w-screen h-screen max-w-none max-h-none p-0 m-0 bg-transparent flex items-end justify-center backdrop:bg-black/50'>
+            <div className='bg-white w-full p-6 pt-10 rounded-t-2xl grid'>
+                <header className=''>
+                    <span className='w-10.5 h-10.5'><IconConfirmed /></span>
+                    <h3 className='text-rose-900 text-preset-1 font-bold leading-tight capitalize'>order confirmed</h3>
+                    <p className='text-rose-500 text-preset-3 font-normal'>We hope you enjoy your food!</p>
+                </header>
+                <div>
+                    <h5 className='text-preset-1 text-amber-600'>{state.items[0].id}</h5>
+                </div>
+                <button type='button' onClick={onClose} className='w-full py-4 bg-red text-white text-preset-3 font-bold capitalize rounded-full'>start new order</button>
+            </div>
+
         </dialog>
     );
 }
